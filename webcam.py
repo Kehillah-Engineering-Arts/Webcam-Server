330516bdd3bfd942f4abdab9804c73227c0b574a
avikl <avi12kl@gmail.com>
2015-11-01 16:29:21 -0800

    Fixed bug with files

--- webcam.py
+++ webcam.py
@@ -1,14 +1,18 @@
 import subprocess
 import datetime
 import time
+import os
 
 directory = "/home/motion"
 while True:
 	current_seconds = datetime.datetime.now().strftime("%S")
 	if ((int(current_seconds))%10)==0:
 		folder = datetime.datetime.now().strftime("%m-%d-%y")
+		fullPath = directory + "/" + folder
 		filename = datetime.datetime.now().strftime("%m-%d-%y-%H:%M:%S")
-		command = "sudo fswebcam -r 640x480 -q " + directory + "/" + folder + "/" + filename + ".jpg"
+		if not os.path.exists(fullPath):
+			os.makedirs(fullPath)
+		command = "sudo fswebcam -r 640x480 -q " + fullPath + "/" + filename + ".jpg"
 		grab_cam = subprocess.Popen(command, shell=True)
 		grab_cam.wait()
 	time.sleep(0.5)

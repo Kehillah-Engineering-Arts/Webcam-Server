8bfd05bb3f2e1e02fbb1a4d0f378c6c572f177a5
Avi Kaplan-Lipkin <Avi@Avis-MacBook-Pro-2.local>
2016-01-26 13:41:05 -0800

    made updates

--- install.sh
+++ install.sh
@@ -6,3 +6,19 @@
 # (if the directory DNE), pull (update) if repo does
 # exist, and finally execute run.sh
 # install.sh (this file) should source .profile
+
+source ~/.profile
+grep "#webcam" ~/.profile
+if [ $? -eq 1 ] # ? will be 1 if grep found nothing
+then
+  # Append to the file
+  echo "#webcam" >> ~/.profile
+  if [ ! -d "$Webcam-Server" ]; then
+  	# Note to self: clone to the correct directory
+ 		echo "git clone https://github.com/Kehillah-Engineering-Arts/Webcam-Server.git" >> ~/.profile
+ 	else
+ 		echo "git pull" >> ~/.profile 
+	fi
+  echo "sudo ./run.sh" >> ~/.profile 
+  sudo reboot
+fi

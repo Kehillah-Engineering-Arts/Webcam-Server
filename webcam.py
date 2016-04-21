2209ea5d249e67866b14d1a732cacbf7a914be1f
Avi Kaplan-Lipkin <Avi@Avis-MacBook-Pro-2.local>
2016-04-21 11:57:16 -0700

    Changed image capture command

--- webcam.py
+++ webcam.py
@@ -12,8 +12,9 @@ while True:
 		filename = datetime.datetime.now().strftime("%m-%d-%y-%H:%M:%S")
 		if not os.path.exists(fullPath):
 			os.makedirs(fullPath)
-		print("Grabbing image: " + fullPath + "/" + filename)
-		command = "sudo fswebcam -r 640x480 -q " + fullPath + "/" + filename + ".jpg"
+		#print("Grabbing image: " + fullPath + "/" + filename)
+		#command = "sudo fswebcam -r 640x480 -q " + fullPath + "/" + filename + ".jpg"
+		command = "sudo fswebcam -r 640x480 " + fullPath + "/" + filename + ".jpg"
 		grab_cam = subprocess.Popen(command, shell=True)
 		grab_cam.wait()
 	time.sleep(0.5)

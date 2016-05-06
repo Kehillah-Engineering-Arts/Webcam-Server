4bab0443dcf778e8833c4d58b51daba180b51b9e
Ben1152000 <bdarnell@2018.kehillah.org>
2016-05-06 16:00:28 -0700

    Add mounted Hard Drive location

--- transfer.py
+++ transfer.py
@@ -2,7 +2,7 @@ import datetime
 import shutil
 
 piDir = "/home/motion"
-destination = "/home/pi/storage"
+destination = "/mnt/USB" #Location where storage device is mounted
 today = datetime.date.today()
 yesterday = today - datetime.timedelta(days=1)
 folder = yesterday.strftime("%m-%d-%y")

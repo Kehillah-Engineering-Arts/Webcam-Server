efb2286741a804034e204a19531e9dcd56e0ebba
Ben1152000 <bdarnell@2018.kehillah.org>
2016-05-17 16:23:59 -0700

    Added Print Statements

--- webcam.py
+++ webcam.py
@@ -8,20 +8,13 @@ directory = "~/motion"
 while True:
 	current_seconds = datetime.datetime.now().strftime("%S")
 	if ((int(current_seconds))%10)==0:
-		print(1)
 		folder = datetime.datetime.now().strftime("%m-%d-%y")
-		print(2)
 		fullPath = directory + "/" + folder
-		print(3)
 		if not os.path.exists(fullPath):
+			print("Avi why!!!!!")
 			os.makedirs(fullPath)
-		print(4)
 		filename = datetime.datetime.now().strftime("%m-%d-%y-%H:%M:%S")
-		print(5)
 		command = "sudo fswebcam -r 640x480 -q " + fullPath + "/" + filename + ".jpg"
-		print(6)
 		grab_cam = subprocess.Popen(command, shell=True)
-		print(7)
 		grab_cam.wait()
-		print(8)
 	time.sleep(0.5)

b0ee5c9104e64e808e1ab089058d74fe60abdb9b
Ben Y <GitYBenY-a4h1@mailnicks.org>
2017-02-03 14:41:02 -0800

    finalize root/sudo checks

--- motion_install_setup.sh
+++ motion_install_setup.sh
@@ -1,13 +1,32 @@
+#!/bin/bash
 # Must be run as root or with sudo
 
 SUDO=''
-if [ "$EUID" -ne 0 ]
+# if statement checks if user ran as root
+if [ "$EUID" -ne 0 ] ; then
+    # while block asks user if they want to continue not as root
     while true; do
-        read -p "You are not root. Do you want to try with sudo? (y/n) " yn
+        read -p "You are not root user. Do you want to try with sudo? (y/n) " yn
         case $yn in
             [Yy]* ) SUDO='sudo'; break;;
             [Nn]* ) exit;;
             * ) echo "Please answer yes or no.";;
         esac
     done
+# if user is root, asks if they want to continue as root
+else
+    while true; do
+        read -p "You are root user. Do you want to continue as root? (y/n) " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+fi
+# if user is not root, checks if they are not 
+if [ $SUDO == 'sudo' ] ; then
+    $SUDO -v
+else
+    echo You are root user.
 fi
